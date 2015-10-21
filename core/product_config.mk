82897e8af5bcf7a44e6264c57b7d102b10e321e4
mike <mikew8978@gmail.com>
2015-10-21 03:38:24 -0400

    Back to the roots AOSiP
    Conflicts:
    	envsetup.sh

--- core/product_config.mk
+++ core/product_config.mk
@@ -180,7 +180,7 @@ include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
 ifneq ($(CUSTOM_BUILD),)
-    all_product_configs := $(shell ls vendor/solo/products/solo_$(CUSTOM_BUILD).mk)
+    all_product_configs := $(shell ls vendor/aosip/products/aosip_$(CUSTOM_BUILD).mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
